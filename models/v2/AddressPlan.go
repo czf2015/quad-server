1d581d731eb0f8acbe5ac96bd948faba9f1024d1
czf <122385930@qq.com>
2021-10-17 18:12:27 +0800

    修改bug

--- models/v2/AddressPlan.go
+++ models/v2/AddressPlan.go
@@ -2,14 +2,13 @@ package models_v2
 
 type AddressPlan struct {
 	Base
-	Pid   string `json:"pid"`
 	UserId                  string `json:"user_id" form:"user_id"`
 	NetworkAddress          string `json:"network_address" form:"network_address"`
-	BitWidth                int `json:"bit_width"`
-	SubnetAddressBeginValue int `json:"subnet_address_begin_value"`
-	PrefixBitWidth          int `json:"prefix_bit_width"`
+	BitWidth                int    `json:"bit_width"`
+	SubnetAddressBeginValue int    `json:"subnet_address_begin_value"`
+	PrefixBitWidth          int    `json:"prefix_bit_width"`
 	Organization            string `json:"organization" form:"organization"`
-	AddressCount            int `json:"address_count"`
+	AddressCount            int    `json:"address_count"`
 	SubnetType              string `json:"subnet_type" form:"subnet_type"`
 	AddressList             string `json:"address_list"`
 }
